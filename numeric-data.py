530ac662312a1937ed05c094237100c06a465a42
OscarAWS <monkhonk107@gmail.com>
2024-05-22 09:41:03 +0200

    109 commit

---
+++ numeric-data.py
@@ -0,0 +1,25 @@
+print("Python has three numeric types: int, float, and complex")
+
+# Creación de una variable
+myValue=1
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+# Presentar el tipo de dato flotante
+myValue=3.14
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+# Presentar el tipo de dato complejo
+myValue=5j
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+# Presentar el tipo de dato booleano
+myValue=False
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
